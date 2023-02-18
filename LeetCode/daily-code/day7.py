d276f3a6f42d36d1d7358186102ec4caf378f129
Leyton <yldhiking@foxmail.com>
2023-02-18 21:52:53 +0800

    day8

--- LeetCode/daily-code/day7.py
+++ LeetCode/daily-code/day7.py
@@ -11,7 +11,7 @@ class Solution:
             for j in row:
                 tp += j
                 tpl.append(tp)
-            sum_col.append(tpl)
+            sum_row.append(tpl)
         for col in zip(*grid):
             col_len += 1
             tp = 0
@@ -19,21 +19,21 @@ class Solution:
             for j in col:
                 tp += j
                 tpl.append(tp)
-            sum_row.append(tpl)
+            sum_col.append(tpl)
 
         max_bc = min(row_len, col_len)
         for bc in range(max_bc, 0, -1):
             for i in range(row_len - bc + 1):
                 for j in range(col_len - bc + 1):
-                    if sum_row[j][i + bc] - sum_row[j][i] == bc\
-                            and sum_row[j + bc - 1][i + bc] - sum_row[j + bc - 1][i] == bc\
-                            and sum_col[i][j + bc] - sum_col[i][j] == bc\
-                            and sum_col[i + bc - 1][j + bc] - sum_col[i + bc - 1][j] == bc:
+                    if sum_col[j][i + bc] - sum_col[j][i] == bc\
+                            and sum_col[j + bc - 1][i + bc] - sum_col[j + bc - 1][i] == bc\
+                            and sum_row[i][j + bc] - sum_row[i][j] == bc\
+                            and sum_row[i + bc - 1][j + bc] - sum_row[i + bc - 1][j] == bc:
                         return bc * bc
         return 0
 
 
 if __name__ == '__main__':
-    grid = [[1,1,1],[1,0,1],[1,1,1]]
+    grid = [[1,1,1],[1,0,1],[1,1,1], [1,1,1]]
     print(Solution().largest1BorderedSquare(grid))
 
